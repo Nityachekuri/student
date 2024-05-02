e80a8ec923cabb9f1aecc5f5bc9dbe4cedc220b2
Nitya <srinityachekuri259@gmail.com>
2024-05-02 19:39:41 +0530

    Initial commit

---
+++ WebApplication7/Controllers/StudentController.cs
@@ -0,0 +1,105 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public ActionResult Index()
+        {
+            using (var db = new SqlConnection(connectionString))
+            {
+                IEnumerable<Student> students = db.Query<Student>("SELECT * FROM Students");
+                return View(students);
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new SqlConnection(connectionString))
+            {
+                Student student = db.QuerySingleOrDefault<Student>("SELECT * FROM Students WHERE StudentID = @StudentID", new { StudentID = id });
+                if (student == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(student);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new SqlConnection(connectionString))
+                {
+                    string query = @"INSERT INTO Students (FirstName, LastName, DateOfBirth, Gender, Address, Email, Phone) 
+                                 VALUES (@FirstName, @LastName, @DateOfBirth, @Gender, @Address, @Email, @Phone)";
+                    db.Execute(query, student);
+                }
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            using (var db = new SqlConnection(connectionString))
+            {
+                Student student = db.QuerySingleOrDefault<Student>("SELECT * FROM Students WHERE StudentID = @StudentID", new { StudentID = id });
+                if (student == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(student);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new SqlConnection(connectionString))
+                {
+                    string query = @"UPDATE Students SET FirstName = @FirstName, LastName = @LastName, 
+                             DateOfBirth = @DateOfBirth, Gender = @Gender, Address = @Address, 
+                             Email = @Email, Phone = @Phone WHERE StudentID = @StudentID";
+                    db.Execute(query, student);
+                }
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (var db = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Students WHERE StudentID = @StudentID";
+                db.Execute(query, new { StudentID = id });
+            }
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ WebApplication7/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication7.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Date of birth is required")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [RegularExpression(@"^[^\s@]+@[^\s@]+\.[^\s@]+$", ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Invalid phone number")]
+        public string Phone { get; set; }
+    }
+
+    }
